7256c13f372148b55edeff0477f077c980d35654
Tharindu Sandeepa <73959253+tharindudsandeepa@users.noreply.github.com>
2023-05-17 12:32:56 +0530

    add gradient container file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,28 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(255, 26, 2, 80),
-        Color.fromARGB(255, 45, 7, 98),
-      ])),
-      child: const Center(
-        child: Text(
-          'Hello Worlds',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
-          ),
-        ),
-      ),
-    );
-  }
 }
